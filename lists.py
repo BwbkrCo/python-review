ccdadf4b055af001882901d2eb99327f64d7a2d1
Brian Lafuente <loulafuente@BrianLafuentes-Macbook-Pro.local>
2019-04-15 17:33:21 -0400

    list review

---
+++ lists.py
@@ -0,0 +1,18 @@
+# Lists
+# created with [ ]
+# hold different values, can be numbers or text
+
+someList = ["some text", 12.9, True, ["nested",8]]
+print someList
+
+blockchain = [1, 8.6, 5.1]
+print blockchain
+
+blockchain = [1, 8.6, 5.1, 10]
+print blockchain
+
+blockchain.append(3)
+print blockchain
+
+blockchain.pop()
+print blockchain
