58baa2614cb43b3f76b48f505bc80fccd0b9dc6a
Brian Lafuente <loulafuente@BrianLafuentes-Macbook-Pro.local>
2019-04-15 17:32:58 -0400

    review

--- variables.py
+++ variables.py
@@ -11,3 +11,26 @@ print is_old
 
 name = 'Brian'
 print name 
+
+# Operators
+op0 = 5 + 10
+op1 = 5 - 10
+op2 = 5 * 10
+op3 = 5 / 10
+op4 = 5 ** 10
+op5 = 5 % 10
+
+print op0
+print op1
+print op2
+print op3
+print op4
+print op5
+
+# Strings 
+name = "Brian"
+text = """
+Multi Line
+Text 
+"""
+print text 
