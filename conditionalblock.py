81f055cd749a2cb9cbecb5cc85941be4e809b93c
Brian Lafuente <loulafuente@lawn-143-215-90-22.lawn.gatech.edu>
2019-04-16 21:07:43 -0400

    editting funcitons

--- conditionalblock.py
+++ conditionalblock.py
@@ -34,23 +34,29 @@ def print_blockchain_elements():
     for block in blockchain:
         print('Outputting Block')
         print(block)
+    else:
+        print('-' * 20)
 
 def verify_chain():
-    block_index = 0
+    # block_index = 0
     is_valid = True
-    for block in blockchain:
+    for block_index in range(len(blockchain)):
         if block_index == 0:
-            block_index += 1
             continue
-        if block[0] == blockchain[block_index - 1]:
+        elif blockchain[block_index][0] == blockchain[block_index - 1]:
             is_valid = True
         else:
             is_valid = False
             break
-        block_index += 1
+
+    # for block in blockchain:
+    #     
+    #     block_index += 1
     return is_valid
 
-while True:
+waiting_for_input = True
+
+while waiting_for_input:
     print("Please choose")
     print("1: Add a new transaction value")
     print("2: Output the blockchain blocks")
@@ -66,7 +72,7 @@ while True:
         if len(blockchain) >=1:
             blockchain[0] = [2]
     elif user_choice == 'q':
-        break
+        waiting_for_input = False
     else:
         print('Input was invalid, please pick a value from the list!')
 
@@ -74,6 +80,7 @@ while True:
     if not verify_chain():
         print("Invalid blockchain")
         break
-    
+else:
+    print('User Left')
 
 print('Done!')
