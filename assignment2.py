2a699ab5aad1adafc4a1bf13bb97af990478dbbd
Brian Lafuente <loulafuente@lawn-143-215-90-22.lawn.gatech.edu>
2019-04-16 22:24:33 -0400

    still working on 2nd assignment

---
+++ assignment2.py
@@ -0,0 +1,16 @@
+# Loops and Conditionals Assignment 
+
+# 1) Create a list of names and use a for loop to output the length of each name (len() ).
+names = ["Lena", "Brian", "Jeff", "David", "Eric", "Adrian", "Cyrus", "Bea", "Nina"]
+for i in range(len(names)):
+    # print(len(names[i]))
+
+    # 2) Add an if check inside the loop to only output names longer than 5 characters.
+    if len(names[i]) > 5:
+        print(names[i])
+    
+    # 3) Add another if check to see whether a name includes a “n”  or “N”  character.
+    
+
+
+# 4) Use a while  loop to empty the list of names (via pop() )
