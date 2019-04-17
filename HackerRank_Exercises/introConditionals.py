d9d7c5fcc99e977895cb4e4ab6481aad9a56a70c
Brian Lafuente <loulafuente@lawn-143-215-90-22.lawn.gatech.edu>
2019-04-17 02:07:46 -0400

    algorithmn extra practice

---
+++ HackerRank_Exercises/introConditionals.py
@@ -0,0 +1,11 @@
+N = int(input("Enter a value: "))
+
+
+if(N % 2 == 0 and (N > 2 and N < 5)):
+    print("Not Weird")
+elif(N % 2 == 0 and (N > 6 and N < 20)):
+    print("Weird")
+elif(N % 2 == 0 and (20 < N)):
+    print("Not Weird")
+elif(N % 2 == 1cd ):
+    print("Weird")
---
+++ twoSum.py
@@ -0,0 +1,11 @@
+nums = [2, 7, 11, 15]
+target = 9
+
+# for i in range(nums):
+#     sum = nums[i] + nums[i+1]
+#     print(sum)
+
+array = [1, 2, 3, 4, 10, 11]
+
+total = sum(array)
+print(total)
