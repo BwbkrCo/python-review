d6ba8929154d061e065c724591aed476242f569b
Brian Lafuente <loulafuente@lawn-143-215-93-170.lawn.gatech.edu>
2019-04-23 22:34:51 -0400

    introConditionals

--- HackerRank_Exercises/introConditionals.py
+++ HackerRank_Exercises/introConditionals.py
@@ -3,7 +3,7 @@ N = int(input("Enter a value: "))
 
 if(N % 2 == 0 and (N > 2 and N < 5)):
     print("Not Weird")
-elif(N % 2 == 0 and (N > 6 and N < 20)):
+elif(N % 2 == 0 and (N > 6 and N <= 20)):
     print("Weird")
 elif(N % 2 == 0 and (20 < N)):
     print("Not Weird")
